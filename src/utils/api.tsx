d07090188a219ddbce9946e489aabf9cc78c62ee
Iwara <naijarace1@gmail.com>
2024-09-01 07:40:47 +0100

    Update api.tsx

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: `https://localhost:5000`,
+    baseURL: `https://tap-backend.vercel.app/api`,
     headers: {
       'Content-Type': 'application/json'
     }

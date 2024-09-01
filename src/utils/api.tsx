96ed62a3da09a6b3943de2a1155a9ab56eca916f
Iwara <naijarace1@gmail.com>
2024-09-01 06:58:29 +0100

    Update api.tsx

--- src/utils/api.tsx
+++ src/utils/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-    baseURL: `https://darrel-clicker-game-backend-1.onrender.com/api`,
+    baseURL: `https://localhost:5000`,
     headers: {
       'Content-Type': 'application/json'
     }
